93ebb5d0cde4a4c758f91881eee0b2963b0f6b9e
Matheus Altrão <matheusaltrao2@gmail.com>
2024-05-08 16:45:43 -0400

    feat: add new project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,86 +1,95 @@
 const projects = [
-    {
-        name: 'Holiday Plan',
-        description: 'Organize as suas férias com Holiday Plan. ',
-        image: 'https://i.pinimg.com/originals/51/51/c1/5151c1af1135cbb166eebf1b96e8a85e.png',
-        link: ' https://holiday-plan.vercel.app/',
-    },
+  {
+    name: 'Holiday Plan',
+    description: 'Plneje e organize as suas férias usando o Holiday Plan. ',
+    image: 'https://i.pinimg.com/originals/51/51/c1/5151c1af1135cbb166eebf1b96e8a85e.png',
+    link: 'https://holiday-plan.vercel.app/',
+  },
 
-    {
-        name: 'Blog',
-        description: 'Conheça mais sobre mim, conto um pouco do meu dia a dia, esse é o meu blog. ',
-        image: 'https://i.pinimg.com/originals/69/80/f7/6980f7af3e36d70c7b3b6d92d5cdbfc8.jpg',
-        link: 'https://matheus-blog-oauq.vercel.app/',
-    },
-    {
-        name: 'Sales',
-        description: 'Plataforma para simplificar seu processo de vendas, Sales.',
-        image: 'https://i.pinimg.com/originals/68/64/f0/6864f04167ebb0edec4d74169ae8f065.png',
-        link: 'https://vendas-pink.vercel.app/',
-    },
-    {
-        name: 'TaskList',
-        description:
-            'Organize melhor o seu dia, listagem de tarefas e compartilhe com os seus amigos.',
-        image: 'https://i.pinimg.com/originals/ad/d3/04/add3043a92810d64512576232b67e32a.jpg',
-        link: 'https://task-list-eta-six.vercel.app/',
-    },
+  {
+    name: 'Kerdos',
+    description:
+      'Tenha controle financeiro na palma da sua mão e fique por dentro de investimentos. ',
+    image: 'https://i.pinimg.com/originals/2c/1d/27/2c1d2765695234ee48c45809ed033bc1.png',
+    link: 'https://kerdos.vercel.app/',
+  },
 
-    {
-        name: 'Games',
-        description: 'Procure os seus games preferidos e veja muitas informações sobre ele.',
-        image: 'https://games-indol-five.vercel.app/_next/image?url=https%3A%2F%2Fsujeitoprogramador.com%2Fnext-api%2Ffoto14.png&w=1920&q=100',
-        link: 'https://games-indol-five.vercel.app/',
-    },
+  {
+    name: 'Blog',
+    description: 'Conheça mais sobre mim, conto um pouco  do meu dia a dia e posto lá. ',
+    image: 'https://i.pinimg.com/originals/69/80/f7/6980f7af3e36d70c7b3b6d92d5cdbfc8.jpg',
+    link: 'https://matheus-blog-oauq.vercel.app/',
+  },
+  {
+    name: 'Sales',
+    description: 'Plataforma para simplificar seu processo de vendas, Sales.',
+    image: 'https://i.pinimg.com/originals/68/64/f0/6864f04167ebb0edec4d74169ae8f065.png',
+    link: 'https://vendas-pink.vercel.app/',
+  },
+  {
+    name: 'TaskList',
+    description: 'Organize melhor o seu dia, listagem de tarefas e compartilhe com os seus amigos.',
+    image: 'https://i.pinimg.com/originals/ad/d3/04/add3043a92810d64512576232b67e32a.jpg',
+    link: 'https://task-list-eta-six.vercel.app/',
+  },
 
-    {
-        name: 'SpiderMan-Miles',
-        description: 'Hero section inspirado no Jogo de PS4 e PS5 Miles Morales.',
-        image: 'https://i.pinimg.com/originals/13/52/b3/1352b36cbf7f5db50bcc6ab2726d205e.jpg',
-        link: 'https://matheusaltrao.github.io/SpiderMan-Miles/',
-    },
+  {
+    name: 'Games',
+    description: 'Procure os seus games preferidos e veja muitas informações sobre ele.',
+    image:
+      'https://games-indol-five.vercel.app/_next/image?url=https%3A%2F%2Fsujeitoprogramador.com%2Fnext-api%2Ffoto14.png&w=1920&q=100',
+    link: 'https://games-indol-five.vercel.app/',
+  },
 
-    {
-        name: 'Ui Notion',
-        description: 'Usando uma lib de editor de texto para simular funcionalidades do notion.',
-        image: 'https://i.pinimg.com/originals/88/dc/1f/88dc1f6b1499ff436354c1fdf11f3f84.png',
-        link: 'https://ui-notion-seven.vercel.app/',
-    },
+  {
+    name: 'SpiderMan-Miles',
+    description: 'Hero section inspirado no Jogo de PS4 e PS5 Miles Morales.',
+    image: 'https://i.pinimg.com/originals/13/52/b3/1352b36cbf7f5db50bcc6ab2726d205e.jpg',
+    link: 'https://matheusaltrao.github.io/SpiderMan-Miles/',
+  },
 
-    {
-        name: 'Store',
-        description: 'Desenvolvendo um carrinho de compras, usando Context.',
-        image: 'https://myportfolio-omega-nine.vercel.app/assets/matheuStore-7f0db33b.png',
-        link: 'https://cart-products-zeta.vercel.app/',
-    },
+  {
+    name: 'Ui Notion',
+    description: 'Usando uma lib de editor de texto para simular funcionalidades do notion.',
+    image: 'https://i.pinimg.com/originals/88/dc/1f/88dc1f6b1499ff436354c1fdf11f3f84.png',
+    link: 'https://ui-notion-seven.vercel.app/',
+  },
 
-    {
-        name: 'Dashboard',
-        description: 'Uma dashboard para visualizações de compras.',
-        image: 'https://myportfolio-omega-nine.vercel.app/assets/dashboard-17dddae2.png',
-        link: 'https://dashboard-3rsk.vercel.app/',
-    },
+  {
+    name: 'Store',
+    description: 'Desenvolvendo um carrinho de compras, usando Context.',
+    image: 'https://myportfolio-omega-nine.vercel.app/assets/matheuStore-7f0db33b.png',
+    link: 'https://cart-products-zeta.vercel.app/',
+  },
 
-    {
-        name: 'Assistência médica',
-        description: 'LandingPage para consultórios médicos, simples e direta ao ponto.',
-        image: 'https://myportfolio-omega-nine.vercel.app/assets/assistenciamedica-ea4c928d.png',
-        link: 'https://matheusaltrao.github.io/DoctorCare/',
-    },
+  {
+    name: 'Dashboard',
+    description: 'Uma dashboard para visualizações de compras.',
+    image: 'https://myportfolio-omega-nine.vercel.app/assets/dashboard-17dddae2.png',
+    link: 'https://dashboard-3rsk.vercel.app/',
+  },
 
-    {
-        name: 'Salão de beleza',
-        description: 'Saúde natural para os seus cabelos e dicas de tratamentos.',
-        image: 'https://myportfolio-omega-nine.vercel.app/assets/salaodebeleza-5a49862d.png',
-        link: 'https://matheusaltrao.github.io/Beautysalon./',
-    },
+  {
+    name: 'Assistência médica',
+    description: 'LandingPage para consultórios médicos, simples e direta ao ponto.',
+    image: 'https://myportfolio-omega-nine.vercel.app/assets/assistenciamedica-ea4c928d.png',
+    link: 'https://matheusaltrao.github.io/DoctorCare/',
+  },
 
-    {
-        name: ' Relax Chill Code',
-        description: 'Cards de músicas para escutar, relaxar e programar.',
-        image: 'https://camo.githubusercontent.com/5b5b3c11207f6bc521644904327e002db466cd54aeaa5828e5183b993ed95660/68747470733a2f2f692e70696e696d672e636f6d2f353634782f39352f30622f39642f39353062396432626266323431393661656437653736393136653334353463652e6a7067',
-        link: 'https://player-music-two.vercel.app/',
-    },
+  {
+    name: 'Salão de beleza',
+    description: 'Saúde natural para os seus cabelos e dicas de tratamentos.',
+    image: 'https://myportfolio-omega-nine.vercel.app/assets/salaodebeleza-5a49862d.png',
+    link: 'https://matheusaltrao.github.io/Beautysalon./',
+  },
+
+  {
+    name: ' Relax Chill Code',
+    description: 'Cards de músicas para escutar, relaxar e programar.',
+    image:
+      'https://camo.githubusercontent.com/5b5b3c11207f6bc521644904327e002db466cd54aeaa5828e5183b993ed95660/68747470733a2f2f692e70696e696d672e636f6d2f353634782f39352f30622f39642f39353062396432626266323431393661656437653736393136653334353463652e6a7067',
+    link: 'https://player-music-two.vercel.app/',
+  },
 ];
 
 export { projects };
